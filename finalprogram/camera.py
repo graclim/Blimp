77f7b77450fc5e77840b4b46ad5b492dfa347f96
Grace Lim <gl487@cornell.edu>
2023-07-19 19:17:38 -0400

    small changes

--- finalprogram/camera.py
+++ finalprogram/camera.py
@@ -1,11 +1,32 @@
-# import the necessary packages
+# import the necessary packages 
+
+print("1")
+
 from picamera.array import PiRGBArray
+
+print("2")
+
 from picamera import PiCamera
+
+print("3")
+
 import time
+
+print("4")
+
 import cv2
+
+print("5")
+
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
+
+print("6")
+
 camera.resolution = (640, 480)
+
+print("7")
+
 # allow the camera to warmup
 time.sleep(0.1)
 while True:
